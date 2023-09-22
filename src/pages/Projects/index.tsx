141d836e5b3865393422effae392057ccc9dd2a9
Henrique Gomes <henricopg11@gmail.com>
2023-09-22 18:29:20 -0300

    config: implement card to project page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,10 +1,20 @@
-import * as C from './styles';
+import { Container, Main, Title, CardsContainer } from './styles';
+import Card from '../../components/Card';
+
+import * as data from '../../data/projects.json';
+const projectsString = JSON.stringify(data);
+const projects = JSON.parse(projectsString).projects;
 
 const Projects = () => {
   return (  
-    <C.Container>
-      <C.Main></C.Main>
-    </C.Container>
+    <Container>
+      <Title>Meus Projetos</Title>
+      <Main>
+        <CardsContainer>
+          <Card projects={projects} />
+        </CardsContainer>
+      </Main>
+    </Container>
   );
 }
 
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,39 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
   color: #fffefd;
 `;
 
 export const Main = styled.main`
-  margin: 125px;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr))
+  place-items: center;
+  width: 100%;
 `;
 
 export const Title = styled.h1`
   padding: 0;
   margin: 0;
+  width: 100%;
   font-size: 72px;
   text-align: left;
   color: #f87b1b;
   filter: drop-shadow(4px 4px 2px rgba(0,0,0,0.35));
 `;
 
-export const Subtitle = styled.h2`
-  padding-right: 81px;
-  margin: 0;
-  text-align: justify;
-`;
-
-export const Text = styled.p`
-  padding: 0;
-  margin: 0;
-  text-align: justify;
-`;
-
-export const Article = styled.article`
-  grid-row: span 4;
-  display: grid;
-  grid-template-rows: subgrid;
+export const CardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  gap: 16px;
 `;
